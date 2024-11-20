7f0158f3ee12403c26e815c9df4f00d41ee3bb41
Ard Biesheuvel <ardb@kernel.org>
2024-11-20 20:36:03 +0100

    efi/libstub: Avoid legacy decompressor zlib/zstd wrappers
    Remove EFI zboot's dependency on the decompression wrappers used by the
    legacy decompressor boot code, which can only process the input in one
    go, and this will not work for upcoming support for embedded ELF images.
    They also do some odd things like providing a barebones malloc()
    implementation, which is not needed in a hosted environment such as the
    EFI boot services.
    
    So instead, implement GZIP deflate and ZSTD decompression in terms of
    the underlying libraries. Support for other compression algoritms has
    already been dropped.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- drivers/firmware/efi/libstub/efistub.h
+++ drivers/firmware/efi/libstub/efistub.h
@@ -4,6 +4,7 @@
 #define _DRIVERS_FIRMWARE_EFI_EFISTUB_H
 
 #include <linux/compiler.h>
+#include <linux/cleanup.h>
 #include <linux/efi.h>
 #include <linux/kernel.h>
 #include <linux/kern_levels.h>
@@ -1053,6 +1054,7 @@ void efi_puts(const char *str);
 __printf(1, 2) int efi_printk(char const *fmt, ...);
 
 void efi_free(unsigned long size, unsigned long addr);
+DEFINE_FREE(efi_pool, void *, if (_T) efi_bs_call(free_pool, _T));
 
 void efi_apply_loadoptions_quirk(const void **load_options, u32 *load_options_size);
 
@@ -1231,5 +1233,8 @@ efi_status_t allocate_unaccepted_bitmap(__u32 nr_desc,
 void process_unaccepted_memory(u64 start, u64 end);
 void accept_memory(phys_addr_t start, unsigned long size);
 void arch_accept_memory(phys_addr_t start, phys_addr_t end);
+
+efi_status_t efi_zboot_decompress_init(unsigned long *alloc_size);
+efi_status_t efi_zboot_decompress(u8 *out, unsigned long outlen);
 
 #endif
